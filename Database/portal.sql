1f91ba4b604e0d38fb4f938e40b16faf5fb4f534
Amicableifti <52049824+Amicableifti@users.noreply.github.com>
2020-03-12 17:46:41 +0600

    Add files via upload

--- Database/portal.sql
+++ Database/portal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2020 at 05:58 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: Mar 12, 2020 at 12:42 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,25 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `action`
---
-
-CREATE TABLE `action` (
-  `id` varchar(5) NOT NULL,
-  `action` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `action`
---
-
-INSERT INTO `action` (`id`, `action`) VALUES
-('1', 'AVAILABLE'),
-('2', 'NOT AVAILABLE');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `course`
 --
 
@@ -52,17 +33,18 @@ CREATE TABLE `course` (
   `cid` int(5) NOT NULL,
   `cname` varchar(1000) NOT NULL,
   `csection` varchar(20) NOT NULL,
-  `ctime` varchar(40) NOT NULL
+  `ctime` varchar(40) NOT NULL,
+  `notice` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `course`
 --
 
-INSERT INTO `course` (`number`, `cid`, `cname`, `csection`, `ctime`) VALUES
-(1, 1112, 'Artificial Intelegence', 'F', '2:00-3:30'),
-(2, 1212, 'ENGINEERING MANAGEMENT', 'F', '11:00-2:00'),
-(3, 1213, 'ENGINEERING MANAGEMENT', 'G', '8:00-4:00');
+INSERT INTO `course` (`number`, `cid`, `cname`, `csection`, `ctime`, `notice`) VALUES
+(1, 1112, 'Artificial Intelegence', 'F', '2:00-3:30', ''),
+(2, 1212, 'ENGINEERING MANAGEMENT', 'F', '11:00-2:00', ''),
+(3, 1213, 'ENGINEERING MANAGEMENT', 'G', '8:00-4:00', '');
 
 -- --------------------------------------------------------
 
@@ -79,18 +61,42 @@ CREATE TABLE `student` (
   `result` varchar(15) NOT NULL,
   `creditcomplete` varchar(20) NOT NULL,
   `cgpa` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL
+  `email` varchar(50) NOT NULL,
+  `ntitle` varchar(100) NOT NULL,
+  `notice` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`no`, `id`, `name`, `cname`, `section`, `result`, `creditcomplete`, `cgpa`, `email`) VALUES
-(1, '17-34013-1', 'Nur,iftikharul Faridee', 'Artificial Intelligence', 'F', '00', '115', '3.81', ''),
-(2, '17-34014-1', 'NUR', 'Engineering Mangement', 'F', '80', '12', '3.3', ''),
-(3, '17-34015-1', 'SHOILY', 'ENGINEERING MANAGEMENT', 'G', '00', '122', '3.61', ''),
-(4, '18-34444-1', 'Ifti', 'ARTIFICIAL INTELEGENCE', 'F', '00', '22', '3.81', 'amicableifti@gmail.c');
+INSERT INTO `student` (`no`, `id`, `name`, `cname`, `section`, `result`, `creditcomplete`, `cgpa`, `email`, `ntitle`, `notice`) VALUES
+(1, '17-34013-1', 'Iftikharul faridee Nur', 'ARTIFICIAL INTELEGENCE', 'F', '70', '333', '3.81', 'amicableifti@gmail.com', 'Quiz time', 'Come at 9:am'),
+(2, '17-3333-1', 'SHOILY', 'COMPUTER NETWORK', 'G', '00', '123', '3.55', 'abc@gmail.com', '--', '--');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload`
+--
+
+CREATE TABLE `upload` (
+  `no` int(5) NOT NULL,
+  `cid` varchar(40) NOT NULL,
+  `notice` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `upload`
+--
+
+INSERT INTO `upload` (`no`, `cid`, `notice`) VALUES
+(1, '1112', 'fv gfv'),
+(2, '1112', 'tb5ynbyrby5revt4'),
+(3, '1212', 'WGSHRRN'),
+(4, '1112', 'hEY HOW aRE you BOY?'),
+(5, '1112', 'fewghtrym,.li/;/po,uymthrgeffrgtnhyjuki'),
+(6, '1112', '4gt4b5tb5y');
 
 -- --------------------------------------------------------
 
@@ -112,8 +118,34 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `username`, `password`, `email`, `type`) VALUES
-(1, 'Ifti', 'ifti', 'ifti', 'amicableifti@gmail.c', 'teacher'),
+(1, 'Iftikharu Faridee Nu', 'ifti', 'ifti', 'abc@gmail.com', 'teacher'),
 (2, 'Ifti', 'alamin', 'alamin', 'amicableifti@gmail.c', 'teacher');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `upload`
+--
+ALTER TABLE `upload`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `upload`
+--
+ALTER TABLE `upload`
+  MODIFY `no` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
